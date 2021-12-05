7c268332e07b2b7e72eabe0afc8ba9abb7d7f37f
Lance ODonnell <lance.odonnell.wd@gmail.com>
2021-12-05 12:37:06 -0700

    Update index.js
    allow all origins using the middleware cors

--- index.js
+++ index.js
@@ -35,16 +35,8 @@ app.use(passport.initialize());
 
 //install cors
 const cors = require('cors');
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
+ 
 
 //import the auth endpoint
 const auth = require('./auth')(app);
